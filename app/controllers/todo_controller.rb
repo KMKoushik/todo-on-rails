e3ee91a2bceed890b7f8a1a4a83b395ec605beb5
Koushik Mohan <koushikmohan1996@gmail.com>
2020-02-04 13:45:43 +0530

    Use current_user.tasks and task.todoitems to improve readability

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,47 +2,32 @@ class TodoController < ApplicationController
 
   # POST /:task_id/todo
   def create
-    todoitem = Todoitem.new(todoitem_params)
-    todoitem.task_id = params[:task_id]
+    todoitem = get_task.todoitems.new(todoitem_params)
     todoitem.is_completed = false
-    task = get_task
-    if task
-      if todoitem.save
-        render json: { message: todoitem }, status: :ok
-      else
-        render json: { message: 'Some error occured' }, status: :internal_server_error
-      end
+    if todoitem.save
+      render json: { message: todoitem }, status: :ok
     else
-      render json: { message: 'Task not found' }, status: :bad_request
+      render json: { message: 'Some error occured' }, status: :internal_server_error
     end
   end
 
   # PUT /:task_id/todo/:id
   def update
-    task = get_task
-    if task
-      todoItem = Todoitem.find_by(id: params[:id], task_id: params[:task_id])
-      if todoItem.update(todoitem_params)
-        render json: { message: todoItem }, status: :ok
-      else
-        render json: { message: 'Some error occured' }, status: :internal_server_error
-      end
+    todoItem = get_task.todoitems.find_by(id: params[:id])
+
+    if todoItem.update(todoitem_params)
+      render json: { message: todoItem }, status: :ok
     else
-      render json: { message: 'Task not found' }, status: :bad_request
+      render json: { message: 'Some error occured' }, status: :internal_server_error
     end
   end
 
   def destroy
-    task = get_task
-    if task
-      todoItem = Todoitem.find_by(id: params[:id], task_id: params[:task_id])
-      if todoItem && todoItem.destroy
-        render json: { message: todoItem }, status: :no_content
-      else
-        render json: { message: 'Some error occured' }, status: :internal_server_error
-      end
+    todoItem = get_task.todoitems.find_by(id: params[:id])
+    if todoItem && todoItem.destroy
+      render json: { message: todoItem }, status: :no_content
     else
-      render json: { message: 'Task not found' }, status: :bad_request
+      render json: { message: 'Some error occured' }, status: :internal_server_error
     end
   end
 
@@ -52,6 +37,6 @@ class TodoController < ApplicationController
   end
 
   def get_task
-    Task.find_by(user_id: current_user.id, id: params[:task_id])
+    current_user.tasks.find_by(id: params[:task_id])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :todoitems
   validates :name, presence: true
 end
